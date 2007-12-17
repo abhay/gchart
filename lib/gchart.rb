2f642b80f1e8ca9d2e305b7662251b171561d24b
jbarnette <jbarnette@97666ce3-63e7-4ded-9107-7b9b1732aa72>
2007-12-17 19:01:57 +0000

    Minor docfixes.
    git-svn-id: http://gchart.rubyforge.org/svn/trunk@27 97666ce3-63e7-4ded-9107-7b9b1732aa72

--- lib/gchart.rb
+++ lib/gchart.rb
@@ -10,14 +10,29 @@ module GChart
   PAIRS = CHARS.collect { |first| CHARS.collect { |second| first + second } }.flatten
 
   class << self
+    # Convenience constructor for GChart::Line.
     def line(*args, &block); Line.new(*args, &block) end
+    
+    # Convenience constructor for GChart::XYLine.
     def xyline(*args, &block); XYLine.new(*args, &block) end
+    
+    # Convenience constructor for GChart::Bar.
     def bar(*args, &block); Bar.new(*args, &block) end    
+    
+    # Convenience constructor for GChart::Pie.
     def pie(*args, &block); Pie.new(*args, &block) end    
+    
+    # Convenience constructor for GChart::Pie3D.
     def pie3d(*args, &block); Pie3D.new(*args, &block) end    
+    
+    # Convenience constructor for GChart::Venn.
     def venn(*args, &block); Venn.new(*args, &block) end    
+    
+    # Convenience constructor for GChart::Scatter.
     def scatter(*args, &block); Scatter.new(*args, &block) end
     
+    # Encode +n+ as a string. +n+ is normalized based on +max+.
+    # +encoding+ can currently only be :extended.
     def encode(encoding, n, max)
       unless encoding == :extended
         raise ArgumentError, "unsupported encoding: #{encoding.inspect}"
--- lib/gchart/base.rb
+++ lib/gchart/base.rb
@@ -3,14 +3,28 @@ require "uri"
 
 module GChart
   class Base
+    # Array of chart data. See subclasses for specific usage.
     attr_accessor :data
+    
+    # Hash of additional HTTP query params.
     attr_accessor :extras
+    
+    # Chart title.
     attr_accessor :title
+    
+    # Array of rrggbb colors, one per data set.
     attr_accessor :colors
+    
+    # Array of legend text, one per data set.
     attr_accessor :legend
+    
+    # Max data value for quantization.
     attr_accessor :max
 
+    # Chart width, in pixels.
     attr_reader :width    
+    
+    # Chart height, in pixels.
     attr_reader :height
 
     def initialize(options={}, &block)
@@ -57,7 +71,7 @@ module GChart
     end
 
     # Writes the chart's generated PNG to a file. If +io_or_file+ quacks like an IO,
-    # +write+ will be called on it instead.
+    # calls +write+ on it instead.
     def write(io_or_file="chart.png")
       return io_or_file.write(fetch) if io_or_file.respond_to?(:write)
       open(io_or_file, "w+") { |io| io.write(fetch) }
@@ -65,7 +79,7 @@ module GChart
 
     protected
     
-    def query_params
+    def query_params #:nodoc:
       params = { "cht" => render_chart_type, "chs" => size }
     
       render_data(params)
@@ -76,11 +90,11 @@ module GChart
       params.merge(extras)
     end
 
-    def render_chart_type
+    def render_chart_type #:nodoc:
       raise NotImplementedError, "override in subclasses"
     end
     
-    def render_data(params)
+    def render_data(params) #:nodoc:
       raw = data && data.first.is_a?(Array) ? data : [data]
       max = self.max || raw.collect { |s| s.max }.max
   
@@ -91,15 +105,15 @@ module GChart
       params["chd"] = "e:#{sets.join(",")}"
     end
     
-    def render_title(params)
+    def render_title(params) #:nodoc:
       params["chtt"] = title.tr("\n", "|").gsub(/\s+/, "+") if title
     end
     
-    def render_colors(params)
+    def render_colors(params) #:nodoc:
       params["chco"] = colors.join(",") if colors  
     end
     
-    def render_legend(params)
+    def render_legend(params) #:nodoc:
       params["chdl"] = legend.join("|") if legend
     end    
   end
