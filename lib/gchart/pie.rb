e7dfb84e42130b2ad0d9d36ce1a6cd341a1b03a2
jbarnette <jbarnette@97666ce3-63e7-4ded-9107-7b9b1732aa72>
2007-12-28 22:10:26 +0000

    Formatting tweaks.
    git-svn-id: http://gchart.rubyforge.org/svn/trunk@43 97666ce3-63e7-4ded-9107-7b9b1732aa72

--- lib/gchart/pie.rb
+++ lib/gchart/pie.rb
@@ -1,9 +1,12 @@
 module GChart
   class Pie < GChart::Base
-    
-    # accepts a single data set for the pie chart. Raises +ArgumentError+ if more than one data set is passed in.
+    # A single array of chart data. Raises +ArgumentError+
+    # if more than one data set is provided.
     def data=(data)
-      raise ArgumentError, "Pie charts only take one set of data" if data.is_a?(Array) and data.first.is_a?(Array) and data.size > 1
+      if data.is_a?(Array) and data.first.is_a?(Array) and data.size > 1
+        raise ArgumentError, "Pie charts only have one data set" 
+      end
+      
       super(data)
     end
     
@@ -11,7 +14,7 @@ module GChart
       "p"
     end
     
-    def render_legend(params)
+    def render_legend(params) #:nodoc:
       params["chl"] = legend.join("|") if legend
     end
   end
