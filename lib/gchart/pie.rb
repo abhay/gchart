94279521b045bb8bb56b6dddd41c1f072d75216d
abhay <abhay@97666ce3-63e7-4ded-9107-7b9b1732aa72>
2007-12-28 20:05:57 +0000

    enforce pie chart spec: pie charts only take one set of data
    git-svn-id: http://gchart.rubyforge.org/svn/trunk@41 97666ce3-63e7-4ded-9107-7b9b1732aa72

--- lib/gchart/pie.rb
+++ lib/gchart/pie.rb
@@ -1,5 +1,12 @@
 module GChart
   class Pie < GChart::Base
+    
+    # accepts a single data set for the pie chart. Raises +ArgumentError+ if more than one data set is passed in.
+    def data=(data)
+      raise ArgumentError, "Pie charts only take one set of data" if data.is_a?(Array) and data.first.is_a?(Array) and data.size > 1
+      super(data)
+    end
+    
     def render_chart_type #:nodoc:
       "p"
     end
--- spec/gchart/pie_spec.rb
+++ spec/gchart/pie_spec.rb
@@ -19,3 +19,11 @@ describe GChart::Pie, "#query_params" do
     @chart.query_params["chl"].should == "foo"
   end
 end
+
+describe GChart::Pie, "#data" do
+  it "complains if you provide more than one set of data" do
+    lambda { GChart::Pie.new(:data => [1, 2, 3]) }.should_not raise_error(ArgumentError)
+    lambda { GChart::Pie.new(:data => [[1, 2, 3]]) }.should_not raise_error(ArgumentError)
+    lambda { GChart::Pie.new(:data => [[1, 2, 3], [3, 2, 1]]) }.should raise_error(ArgumentError)
+  end
+end
