6db6eaf8be8338cfa2058f0b33be76f47fe953dc
abhay <abhay@97666ce3-63e7-4ded-9107-7b9b1732aa72>
2007-12-28 09:38:20 +0000

    improving test coverage for GCHart::Pie#render_legend
    git-svn-id: http://gchart.rubyforge.org/svn/trunk@38 97666ce3-63e7-4ded-9107-7b9b1732aa72

--- spec/gchart/pie_spec.rb
+++ spec/gchart/pie_spec.rb
@@ -5,3 +5,17 @@ describe GChart::Pie do
     GChart::Pie.new.render_chart_type.should == "p"
   end
 end
+
+describe GChart::Pie, "#query_params" do
+  before(:each) { @chart = GChart::Pie.new }
+  
+  it "contains the chart's type" do
+    @chart.query_params["cht"].should == "p"
+  end
+  
+  it "contains the chart's legend" do
+    @chart.legend = ["foo"]
+    @chart.query_params.keys.include?("chl").should be_true
+    @chart.query_params["chl"].should == "foo"
+  end
+end
